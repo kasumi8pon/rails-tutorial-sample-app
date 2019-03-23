d03fe4d98a44c339586a52e8c1f7f1c0576700b0
kasumi8pon <all.splashing@gmail.com>
2019-03-23 19:06:00 +0900

    Add account activation

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,6 +26,7 @@ Rails.application.configure do
   config.action_dispatch.show_exceptions = false
 
   # Disable request forgery protection in test environment.
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_controller.allow_forgery_protection = false
   config.action_mailer.perform_caching = false
 
